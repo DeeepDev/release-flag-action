ab0c68ecf18f282080a4278fa8304100e64208a9
mehdi <amirabody.dev@gmail.com>
2022-12-01 12:04:57 +0330

    feat: create initial script for banner generation
    1. configure rollup
    2. create action metadata

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export {};
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+try {
+  const testInput = core.getInput("test-input");
+  console.log(testInput);
+  core.setOutput("test-output", "a value for test-output");
+  console.log(github.context);
+} catch (error) {
+  if (error instanceof Error) core.setFailed(error.message);
+}
